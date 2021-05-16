654ae0580847c0ec28df5ea7118bdb7355f7e482
Danijel Jovanović <danijel.xda@gmail.com>
2021-05-16 09:41:00 +0200

    String to number
    Convert string to any number from base 2 to 10
    Needs fix for negative numbers

---
+++ 08 Interna konverzija/String u Interni/glavni.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+unsigned int RUNPP_REG_ERR = 0;
+
+int str_to_inter(char* str, unsigned int* greska, unsigned int baza);
+void konvertujBrojUString(unsigned int broj, char *izrazZaIspis, unsigned int baza); // dopuna programa
+
+int main() {
+    unsigned int r;
+    char s[30]={0};
+    char broj[30]={0};
+    unsigned int g, baza;
+    printf("Unesite brojnu osnovu sistema: ");
+    scanf("%u", &baza);
+    printf("Unesite broj u brojevnoj osnovi %u: ", baza);
+    scanf(" %30[^\n]s",s);
+    r = str_to_inter(s,&g, baza);
+    konvertujBrojUString(r, broj, baza); // dopuna programa
+    printf("\nGreska: %u\n",g);
+    if (g == 0)
+        printf("Rezultat (u decimalnoj osnovi): %u\n",r);
+	// else if(g == 1)
+	//	printf("Broj %s nije unet u binarnoj bazi!\n", s); 
+    #ifdef LEVEL42
+    printf("\nRUNPP_REG_ERR:%d\n",RUNPP_REG_ERR);
+    #endif
+    return ((g<0)||(g>127))?127:g;
+}
+
