70d343f56a9852be9183c33685083547c48b37a5
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-06-01 11:56:33 +0200

    Clean up

--- 10 Domaći zadatak/AR 2021 Domaći/testiraj.sh
+++ 10 Domaći zadatak/AR 2021 Domaći/testiraj.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-# testiranje rešenja zadatka sa unapred definisanim ulazima
-
-#   Copyright 2019 Žarko Živanov
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# exit 0 - stiglo se do testova
-# exit 1 - greška u kompajliranju
-# exit 2 - nije nađen fajl
-
+# clear terminal
 clear
 
 # provera instaliranih programa
